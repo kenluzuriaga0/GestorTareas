a1542a474c6b02c1d98b9da6a66e681716c7c7eb
Ken Luzuriaga <kenluzuriaga0@gmail.com>
2021-02-06 02:14:52 -0500

    inicio con bootsfaces

--- target/generated-sources/annotations/entities/Usuario_.java
+++ target/generated-sources/annotations/entities/Usuario_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-01-31T00:07:25")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-02-06T02:14:07")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
